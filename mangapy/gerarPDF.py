a801e5f85d0e482e15deff63a0bfc0d04b7e2caa
Leonardo Rocha <58191102+leonardosantosrocha@users.noreply.github.com>
2022-05-02 07:54:29 -0300

    MangaPy - Get PDF
    Creating a PDF file after downloading all images.

--- mangapy/gerarPDF.py
+++ mangapy/gerarPDF.py
@@ -1,9 +1,3 @@
-# Developed by
-#   -> Leonardo Rocha
-
-# Target
-#   -> Reading jpg files on my computer and creating PDF files
-
 import os
 from PIL import Image
 
@@ -22,11 +16,11 @@ def gen_file(path:str, chapter:int) -> bool:
             images.append(image)
             i += 1
         if chapter >= 0 and chapter <= 9:
-            fileName = f"BOR_00{chapter}.pdf"
+            fileName = f"KNY_00{chapter}.pdf"
         if chapter >= 10 and chapter <= 99:
-            fileName = f"BOR_0{chapter}.pdf"
+            fileName = f"KNY_0{chapter}.pdf"
         if chapter >= 100 and chapter <= 999:
-            fileName = f"BOR_{chapter}.pdf"
+            fileName = f"KNY_{chapter}.pdf"
         image.save(fileName, save_all=True, append_images=images) 
     except:
         print("All pdf have been generated!")
@@ -34,10 +28,10 @@ def gen_file(path:str, chapter:int) -> bool:
 def main():
     chapter = 1
     while True:
-        path = fr"C:\Users\Leonardo Rocha\Desktop\Leonardo\Python Projetos\download_img\chapter {chapter}"
+        path = fr"{os.getcwd()}+\chapter {chapter}"
         gen_file(path, chapter)
         chapter += 1
-        if chapter > 6:
+        if chapter == 209:
             break
 
 main()  
