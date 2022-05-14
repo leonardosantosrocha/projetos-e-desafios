5a680fc8527fd0bbf48f9980da84ace9281eac4e
Leonardo Rocha <58191102+leonardosantosrocha@users.noreply.github.com>
2022-05-13 23:15:31 -0300

    Mangapy - Get Image
    Second version of Get Image from Mangapy project:
    - Adding functions: get_image, chapter_treatment, image_treatment, download and how_to_use;

--- mangapy/getImage.py
+++ mangapy/getImage.py
@@ -1,68 +1,110 @@
 import os
-import requests 
+import requests
 import shutil
-import time
 
-CURRENT_DIR = os.getcwd()
 
-def download(url:str, downloadPath:str, image:str) -> bool:
-    response = url
-    # Checking if the url is valid - True
+current_dir = os.getcwd()
+
+
+def get_image(gi_url: object, gi_download_path: str, gi_current_chapter: int, gi_current_image: int) -> bool:
+    response = gi_url
     if response.status_code == 200:
         response.raw.decode_context = True
-        downloadPath = fr"{downloadPath}\{image}.jpg"
-        with open(downloadPath, "wb") as file:
+        download_path = fr"{gi_download_path}/{gi_current_image}.jpg"
+        with open(download_path, "wb") as file:
             shutil.copyfileobj(response.raw, file)
-        print(f"Image has been downloaded!")
+        print(f"IMAGE {gi_current_image} FROM CHAPTER {gi_current_chapter} HAS BEEN DOWNLOADED!")
         return True
-    # Checking if the url is valid - False
-    else: 
-        print(f"All images have been downloaded!\n")
+    else:
+        print(f"ALL IMAGES HAVE BEEN DOWNLOADED!\n")
         return False
-    
-def main():
-    # Initial chapter
-    chapter = 1
-    while True:
-        # Chapter treatment - adding 0 to chapter
-        if chapter <= 9:
-            requestToChapter = requests.get(f"https://mangayabu.top/mangas2/kimetsu-no-yaiba/capitulo-0{chapter}/", stream = True)
-        # Chater treatment - pass
-        else:
-            requestToChapter = requests.get(f"https://mangayabu.top/mangas2/kimetsu-no-yaiba/capitulo-{chapter}/", stream = True)
-        # If the chapter doesnt exist
-        if requestToChapter.status_code == 404:
-            print(f"The chapter doesn't exist!")
-            break
-        # If the chapter exists
-        else:
-            image = 1
-            os.mkdir(fr"{CURRENT_DIR}\chapter {chapter}")
-            while True:
-                # Image treatment - adding 0 to chapter and image
-                if chapter <= 9 and image <= 9:
-                    requestToImage = requests.get(f"https://mangayabu.top/mangas2/kimetsu-no-yaiba/capitulo-0{chapter}/0{image}.jpg", stream = True)
-                # Image treatment - adding 0 to chapter
-                elif chapter <= 9 and image > 9:
-                    requestToImage = requests.get(f"https://mangayabu.top/mangas2/kimetsu-no-yaiba/capitulo-0{chapter}/{image}.jpg", stream = True)
-                # Image treatment - adding 0 to image
-                elif chapter > 9 and image <= 9:
-                    requestToImage = requests.get(f"https://mangayabu.top/mangas2/kimetsu-no-yaiba/capitulo-{chapter}/0{image}.jpg", stream = True)
-                # Image treatment - pass
-                else:
-                    requestToImage = requests.get(f"https://mangayabu.top/mangas2/kimetsu-no-yaiba/capitulo-{chapter}/{image}.jpg", stream = True)
-                # Applying the function download to capture the images
-                try:
-                    returnDownload = download(requestToImage, fr"{CURRENT_DIR}\chapter {chapter}", image)
-                # Treatment unexpected errors
-                except:
-                    print("Error")
-                if returnDownload == False:
-                    break
-                image += 1
-            # Final chapter
-            if chapter == 209:
+
+
+def chapter_treatment(ct_url: str, ct_current_chapter: int) -> str:
+    if ct_current_chapter <= 9:
+        return f"{ct_url}/capitulo-0{ct_current_chapter}/"
+    else:
+        return f"{ct_url}/capitulo-{ct_current_chapter}/"
+
+
+def image_treatment(it_url: str, it_current_chapter: int, it_current_image: int) -> str:
+    if it_current_chapter <= 9 and it_current_image <= 9:
+        return f"{it_url}/capitulo-0{it_current_chapter}/0{it_current_image}.jpg"
+    elif it_current_chapter <= 9 and it_current_image > 9:
+        return f"{it_url}/capitulo-0{it_current_chapter}/{it_current_image}.jpg"
+    elif it_current_chapter > 9 and it_current_image <= 9:
+        return f"{it_url}/capitulo-{it_current_chapter}/0{it_current_image}.jpg"
+    else:
+        return f"{it_url}/capitulo-{it_current_chapter}/{it_current_image}.jpg"
+
+
+def download():
+    # Defining the current chapter
+    current_chapter = int(1)
+    # User should type the url using the following pattern: https://cdn.mangayabu.top/mangas/naruto
+    url = str(input("TYPE THE URL: ")).lower()
+    # User should define the last chapter, it will be used to stop the downloading loop
+    last_chapter = int(input("TYPE THE LAST CHAPTER: "))
+
+    # While current chapter less than last chapter, we will download the images
+    while current_chapter <= last_chapter:
+        # Using the chapter_treatment function to organize our url
+        url_chapter = chapter_treatment(url, current_chapter)
+        # Verifying if the current chapter is available
+        request_to_chapter = requests.get(url_chapter, stream=True)
+        # If the current chapter doesn't exist
+        if request_to_chapter.status_code == 404:
+            print(f"THE CHAPTER DOESN'T EXIST!")
+            # User should choose stop or continue the execution
+            option = str(input("WOULD YOU LIKE TO TRY THE NEXT CHAPTER (Y/N): "))
+            # Checking the next chapter
+            if option.upper() == "Y":
+                current_chapter += int(1)
+            # Stopping the program
+            else:
                 break
-            chapter += 1
-            
+        # If the current chapter exists
+        else:
+            # Defining the current image
+            current_image = int(1)
+            # Creating a new directory to store the current chapter
+            os.mkdir(fr"{current_dir}/chapter {current_chapter}")
+            while True:
+                # Using the image_treatment function to organize our url
+                url_image = image_treatment(url, current_chapter, current_image)
+                # Verifying if the current chapter is available
+                request_to_image = requests.get(url_image, stream=True)
+                # Trying to get image based on treated url, directory, current chapter and current image
+                try:
+                    ret_get_image = get_image(request_to_image, fr"{current_dir}/chapter {current_chapter}",
+                                              current_chapter, current_image)
+                    # If get_image return is false, we should stop the program
+                    if ret_get_image == False:
+                        break
+                # Treating exceptions
+                except NameError:
+                    print(NameError)
+                current_image += 1
+            current_chapter += 1
+
+
+def how_to_use():
+    print("====================================== WELCOME TO MANGAPY ======================================\n"
+          "| 1 - ACCESS 'HTTPS://MANGAYABU.TOP/'                                                          |\n"
+          "| 2 - CLICK ON YOUR FAVORITE MANGA'S NAME                                                      |\n"
+          "| 3 - SELECT THE FIRST CHAPTER                                                                 |\n"
+          "| 4 - RIGHT BUTTON ON THE FIRST IMAGE AND OPEN IMAGE IN A NEW TAB                              |\n"
+          "| 5 - YOU WILL GOT A LINK LIKE 'HTTPS://CDN.MANGAYABU.TOP/MANGAS/NARUTO/CAPITULO-01/00.JPG'    |\n"
+          "| 6 - CUT THE FIRST PART 'HTTPS://CDN.MANGAYABU.TOP/MANGAS/NARUTO/'                            |\n"
+          "| 7 - PAST THE NEW ADDRESS ON OUR PROGRAM AND PRESS ENTER                                      |\n"
+          "| 8 - SELECT HOW MANY CHAPTERS WOULD YOU LIKE TO DOWNLOAD AND PRESS ENTER                      |\n"
+          "| 9 - ENJOY IT                                                                                 |\n"
+          "============================ DEVELOPED BY: LEONARDO SANTOS DA ROCHA ============================\n")
+
+
+def main():
+    how_to_use()
+    download()
+
+
 main()
